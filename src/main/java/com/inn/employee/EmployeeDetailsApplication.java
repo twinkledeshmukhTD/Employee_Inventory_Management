1414cda502beb185b814c5a7c8710c8a1d9cc21b
twinkle.deshmukh <twinkle.deshmukh@innoeye.com>
2020-08-03 01:12:56 +0530

    added employee structure

---
+++ src/main/java/com/inn/employee/EmployeeDetailsApplication.java
@@ -0,0 +1,13 @@
+package com.inn.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeDetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeDetailsApplication.class, args);
+	}
+
+}
